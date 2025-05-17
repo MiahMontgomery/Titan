f94af4f53f2128919656a35e590839a385171e5c
MiahMontgomery <miahmont@outlook.com>
2025-05-17 11:56:37 -0400

    Update vite.config.ts 4000

--- vite.config.ts
+++ vite.config.ts
@@ -36,9 +36,9 @@ export default async () => {
       port: 4000,
       host: true,
       proxy: {
-        '/api': 'http://localhost:5000',
+        '/api': 'http://localhost:4000',
         '/ws': {
-          target: 'ws://localhost:5000',
+          target: 'ws://localhost:4000',
           ws: true,
         },
       },
